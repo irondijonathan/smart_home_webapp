56fad4609f90e8cd1a5600609b3c5e334de9daf4
Kcee <80968110+irondijonathan@users.noreply.github.com>
2023-07-23 15:57:58 +0000

    test

--- app.py
+++ app.py
@@ -1,27 +1,92 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, current_app
 import requests
-import datetime
+#import datetime
+from datetime import datetime, date, time
+import time
+import threading
+from threading import Lock
+
+render_lock = Lock()
 
 app = Flask(__name__)
+
+
+
+
 
 # Endpoint for controlling the local device
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/control', methods=['POST'])
+'''@app.route('/control', methods=['POST'])
 def control():
-    data = request.get_json()  # Get the JSON data sent from the HTML
+    data = request.get_json()
+    device = data['device']
+    action = data['action']
+    turn_on_time_str = data['turn_on_time']
+    turn_off_time_str = data['turn_off_time']
+    print(data)
+    
 
-    device = data['device']  # Extract the device value
-    action = data['action']  # Extract the action value
-    #print(device)
-    #print(action)
 
-    # Process the device and action values as needed
-    # ...
+    # Get the current date
+    current_date = date.today()
+
+    # Convert turn-on and turn-off times to time objects
+    turn_on_time = datetime.strptime(turn_on_time_str, "%I:%M %p").time()
+    turn_off_time = datetime.strptime(turn_off_time_str, "%I:%M %p").time()
+
+    # Combine the time with the current date to get the datetime objects
+    turn_on_time = datetime.combine(current_date, turn_on_time)
+    turn_off_time = datetime.combine(current_date, turn_off_time)
+
+
+    #print(turn_on_time)
+    #print(turn_off_time)
+
+    # Schedule the notifications for turn-on and turn-off times
+    schedule_notification(device, 'on', turn_on_time)
+    schedule_notification(device, 'off', turn_off_time)
+
+    # Other code to handle the action and perform desired tasks
 
     return 'Device: ' + device + ', Action: ' + action
+
+
+def call_javascript_function(device, action):
+    # Call the JavaScript function to display the notification
+    js_code = "displayNotification('" + device + "', '" + action + "')"
+    script_tag = "<script>" + js_code + "</script>"
+    response = script_tag
+    print(script_tag)
+    #print(True)
+
+
+    # Set the necessary Flask configuration to build URLs
+    app.config['SERVER_NAME'] = '127.0.0.1:8080'
+    app.config['APPLICATION_ROOT'] = '/'
+    app.config['PREFERRED_URL_SCHEME'] = 'http'
+
+    print(app.config['SERVER_NAME'])
+
+    # Get the current Flask application context
+    with app.app_context():
+        # Render the template within the application context
+        return render_template('index.html', js_code=js_code)
+
+def schedule_notification(device, action, scheduled_time):
+    current_time = datetime.now()
+    time_difference = scheduled_time - current_time
+    #print(time_difference)
+
+    if time_difference.total_seconds() > 0:
+        # Create a thread to call the JavaScript function at the scheduled time
+        timer_thread = threading.Timer(time_difference.total_seconds(), call_javascript_function, args=[device, action])
+        timer_thread.start()
+        print("testing")
+'''
+
 
 # Endpoint for forwarding the device and action information to the provided IP
 @app.route('/forward', methods=['POST'])
@@ -30,8 +95,8 @@ def forward():
 
     device = data['device']  # Extract the device value
     action = data['action']  # Extract the action value
-    print(device)
-    print(action)
+    #print(device)
+    #print(action)
 
     # Forward the device and action information to the provided IP
     ip_address = 'http://172.20.10.2:5000/control-device'  # Replace with the appropriate IP address
@@ -60,6 +125,4 @@ def log_data(device, action):
         file.write(log_entry + "\n")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-
+    app.run(host='127.0.0.1', port=8080)
---
+++ static/notify.js
@@ -0,0 +1,26 @@
+function displayNotification(device, action) {
+    var notificationDiv = document.getElementById('notification');
+    var messageElement = document.getElementById('notification-message');
+    var onButton = document.getElementById('notification-on-button');
+    var offButton = document.getElementById('notification-off-button');
+
+    // Set the notification message
+    messageElement.textContent = 'Turn ' + action + ' ' + device + '?';
+
+    // Show the notification pop-up
+    notificationDiv.style.display = 'block';
+
+    // Add event listeners to the buttons
+    onButton.addEventListener('click', function() {
+        sendAction(device, 'on');
+        notificationDiv.style.display = 'none'; // Hide the notification after user action
+    });
+
+    offButton.addEventListener('click', function() {
+        sendAction(device, 'off');
+        notificationDiv.style.display = 'none'; // Hide the notification after user action
+    });
+
+    console.log('Notification displayed:', device, action);
+    console.log('XXXXXXXXXXXX')
+}
